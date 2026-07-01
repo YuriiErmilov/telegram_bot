1a8f7568330d4128fa8430f8368d8ab4b828aa96
Юрий <yurikbc3@gmail.com>
2026-07-01 14:51:13 +0800

    FirstPullRequest

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +28,17 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
+            if(update.message() == null || update.message().text() == null) {
+                return;
+            }
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            String message = update.message().text();
+            if(message.equals("/start")) {
+                long chatId = update.message().chat().id();
+                telegramBot.execute(
+                        new SendMessage(chatId,"Здорова кореш!")
+                );
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,56 @@
+package pro.sky.telegrambot.model;
+
+
+import liquibase.pro.packaged.L;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "notification_task")
+public class NotificationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "chat_id")
+    private Long chatId;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "notification_time")
+    private LocalDateTime notificationTime;
+
+    public NotificationTask() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getNotificationTime() {
+        return notificationTime;
+    }
+    public void setNotificationTime(LocalDateTime notificationTime) {
+        this.notificationTime = notificationTime;
+    }
+
+
+}
